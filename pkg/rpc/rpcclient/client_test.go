43408d24e780dddaccb2101b706cbdb922a3b3fe
Fumihiro Ito <fmhrit@gmail.com>
2020-02-18 22:41:41 +0900

    pkg/rpc: Add some tests

---
+++ pkg/rpc/rpcclient/client_test.go
@@ -0,0 +1,131 @@
+package rpcclient
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/f110/lagrangian-proxy/pkg/rpc/rpctestutil"
+)
+
+func TestNewWithClient(t *testing.T) {
+	a := rpctestutil.NewAdminClient()
+	cl := rpctestutil.NewClusterClient()
+	auth := rpctestutil.NewAuthorityClient()
+	ca := rpctestutil.NewCertificateAuthorityClient()
+	c := NewWithClient(a, cl, auth, ca)
+	if c == nil {
+		t.Fatal("NewWithClient should return a value")
+	}
+
+	_ = c.AddUser("", "")
+	if a.UserAddCall != 1 {
+		t.Error("Expect call UserAdd")
+	}
+	_ = c.DeleteUser("", "")
+	if a.UserDelCall != 1 {
+		t.Error("Expect call UserDel")
+	}
+	_, _ = c.ListAllUser()
+	if a.UserListCall != 1 {
+		t.Error("Expect call UserList")
+	}
+	_, _ = c.ListServiceAccount()
+	if a.UserListCall != 2 {
+		t.Error("Expect call UserList")
+	}
+	_, _ = c.ListUser("")
+	if a.UserListCall != 3 {
+		t.Error("Expect call UserList")
+	}
+	_ = c.NewServiceAccount("", "")
+	if a.UserAddCall != 2 {
+		t.Error("Expect call UserAdd")
+	}
+	_, _ = c.GetUser("", false)
+	if a.UserGetCall != 1 {
+		t.Error("Expect call UserGet")
+	}
+	_ = c.UserBecomeMaintainer("", "")
+	if a.BecomeMaintainerCall != 1 {
+		t.Error("Expect call BecomeMaintainer")
+	}
+	_ = c.ToggleAdmin("")
+	if a.ToggleAdminCall != 1 {
+		t.Error("Expect call ToggleAdmin")
+	}
+	_, _ = c.NewToken("", "")
+	if a.TokenNewCall != 1 {
+		t.Error("Expect call TokenNew")
+	}
+	_, _ = c.ListRole()
+	if a.RoleListCall != 1 {
+		t.Error("Expect call RoleList")
+	}
+	_, _ = c.ListAllBackend()
+	if a.BackendListCall != 1 {
+		t.Error("Expect call BackendList")
+	}
+	_, _ = c.ListAgentBackend()
+	if a.BackendListCall != 2 {
+		t.Error("Expect call BackendList")
+	}
+
+	_, _ = c.ClusterMemberList()
+	if cl.MemberListCall != 1 {
+		t.Error("Expect call MemberList")
+	}
+	_, _ = c.ListConnectedAgent()
+	if cl.AgentListCall != 1 {
+		t.Error("Expect call AgentList")
+	}
+	_, _ = c.Defragment()
+	if cl.DefragmentDatastoreCall != 1 {
+		t.Error("Expect call DefragmentDatastore")
+	}
+
+	_, _ = c.ListCert()
+	if ca.GetSignedListCall != 1 {
+		t.Error("Expect call GetSignedList")
+	}
+	_, _ = c.ListRevokedCert()
+	if ca.GetRevokedListCall != 1 {
+		t.Error("Expect call ListRevokedCert")
+	}
+	_ = c.NewCert("", "", 0, "", "")
+	if ca.NewClientCertCall != 1 {
+		t.Error("Expect call NewClientCert")
+	}
+	_ = c.NewCertByCSR("", "")
+	if ca.NewClientCertCall != 2 {
+		t.Error("Expect call NewClientCert")
+	}
+	_ = c.NewAgentCert("", "")
+	if ca.NewClientCertCall != 3 {
+		t.Error("Expect call NewClientCert")
+	}
+	_ = c.NewAgentCertByCSR("", "'")
+	if ca.NewClientCertCall != 4 {
+		t.Error("Expect call NewClientCert")
+	}
+	_, _ = c.NewServerCert([]byte{})
+	if ca.NewServerCertCall != 1 {
+		t.Error("Expect call NewServerCert")
+	}
+	_ = c.RevokeCert(big.NewInt(0))
+	if ca.RevokeCall != 1 {
+		t.Error("Expect call Revoke")
+	}
+	_, _ = c.GetCert(big.NewInt(0))
+	if ca.GetCall != 1 {
+		t.Error("Expect call Get")
+	}
+
+	_, _ = c.SignRequest("")
+	if auth.SignRequestCall != 1 {
+		t.Error("Expect call SignRequest")
+	}
+	_, _ = c.GetPublicKey()
+	if auth.GetPublicKeyCall != 1 {
+		t.Error("Expect call GetPublicKey")
+	}
+}
--- pkg/rpc/rpctestutil/client.go
+++ pkg/rpc/rpctestutil/client.go
@@ -2,6 +2,7 @@ package rpctestutil
 
 import (
 	"context"
+	"sync"
 
 	"google.golang.org/grpc"
 
@@ -9,6 +10,10 @@ import (
 )
 
 type AuthorityClient struct {
+	sync.Mutex
+
+	SignRequestCall  int
+	GetPublicKeyCall int
 }
 
 func NewAuthorityClient() *AuthorityClient {
@@ -16,9 +21,233 @@ func NewAuthorityClient() *AuthorityClient {
 }
 
 func (a *AuthorityClient) SignRequest(_ context.Context, in *rpc.RequestSignRequest, opts ...grpc.CallOption) (*rpc.ResponseSignResponse, error) {
+	a.Lock()
+	defer a.Unlock()
+	a.SignRequestCall++
+
 	return &rpc.ResponseSignResponse{}, nil
 }
 
 func (a *AuthorityClient) GetPublicKey(ctx context.Context, in *rpc.RequestGetPublicKey, opts ...grpc.CallOption) (*rpc.ResponseGetPublicKey, error) {
-	panic("implement me")
+	a.Lock()
+	defer a.Unlock()
+	a.GetPublicKeyCall++
+
+	return &rpc.ResponseGetPublicKey{}, nil
+}
+
+type AdminClient struct {
+	sync.Mutex
+
+	PingCall             int
+	UserListCall         int
+	UserAddCall          int
+	UserDelCall          int
+	UserGetCall          int
+	BecomeMaintainerCall int
+	ToggleAdminCall      int
+	TokenNewCall         int
+	RoleListCall         int
+	BackendListCall      int
+}
+
+func NewAdminClient() *AdminClient {
+	return &AdminClient{}
+}
+
+func (a *AdminClient) Ping(ctx context.Context, in *rpc.RequestPing, opts ...grpc.CallOption) (*rpc.ResponsePong, error) {
+	a.Lock()
+	defer a.Unlock()
+	a.PingCall++
+
+	return &rpc.ResponsePong{}, nil
+}
+
+func (a *AdminClient) UserList(ctx context.Context, in *rpc.RequestUserList, opts ...grpc.CallOption) (*rpc.ResponseUserList, error) {
+	a.Lock()
+	defer a.Unlock()
+	a.UserListCall++
+
+	return &rpc.ResponseUserList{}, nil
+}
+
+func (a *AdminClient) UserAdd(ctx context.Context, in *rpc.RequestUserAdd, opts ...grpc.CallOption) (*rpc.ResponseUserAdd, error) {
+	a.Lock()
+	defer a.Unlock()
+	a.UserAddCall++
+
+	return &rpc.ResponseUserAdd{}, nil
+}
+
+func (a *AdminClient) UserDel(ctx context.Context, in *rpc.RequestUserDel, opts ...grpc.CallOption) (*rpc.ResponseUserDel, error) {
+	a.Lock()
+	defer a.Unlock()
+	a.UserDelCall++
+
+	return &rpc.ResponseUserDel{}, nil
+}
+
+func (a *AdminClient) UserGet(ctx context.Context, in *rpc.RequestUserGet, opts ...grpc.CallOption) (*rpc.ResponseUserGet, error) {
+	a.Lock()
+	defer a.Unlock()
+	a.UserGetCall++
+
+	return &rpc.ResponseUserGet{}, nil
+}
+
+func (a *AdminClient) BecomeMaintainer(ctx context.Context, in *rpc.RequestBecomeMaintainer, opts ...grpc.CallOption) (*rpc.ResponseBecomeMaintainer, error) {
+	a.Lock()
+	defer a.Unlock()
+	a.BecomeMaintainerCall++
+
+	return &rpc.ResponseBecomeMaintainer{}, nil
+}
+
+func (a *AdminClient) ToggleAdmin(ctx context.Context, in *rpc.RequestToggleAdmin, opts ...grpc.CallOption) (*rpc.ResponseToggleAdmin, error) {
+	a.Lock()
+	defer a.Unlock()
+	a.ToggleAdminCall++
+
+	return &rpc.ResponseToggleAdmin{}, nil
+}
+
+func (a *AdminClient) TokenNew(ctx context.Context, in *rpc.RequestTokenNew, opts ...grpc.CallOption) (*rpc.ResponseTokenNew, error) {
+	a.Lock()
+	defer a.Unlock()
+	a.TokenNewCall++
+
+	return &rpc.ResponseTokenNew{}, nil
+}
+
+func (a *AdminClient) RoleList(ctx context.Context, in *rpc.RequestRoleList, opts ...grpc.CallOption) (*rpc.ResponseRoleList, error) {
+	a.Lock()
+	defer a.Unlock()
+	a.RoleListCall++
+
+	return &rpc.ResponseRoleList{}, nil
+}
+
+func (a *AdminClient) BackendList(ctx context.Context, in *rpc.RequestBackendList, opts ...grpc.CallOption) (*rpc.ResponseBackendList, error) {
+	a.Lock()
+	defer a.Unlock()
+	a.BackendListCall++
+
+	return &rpc.ResponseBackendList{}, nil
+}
+
+type ClusterClient struct {
+	sync.Mutex
+
+	MemberListCall          int
+	MemberStatCall          int
+	AgentListCall           int
+	DefragmentDatastoreCall int
+}
+
+func NewClusterClient() *ClusterClient {
+	return &ClusterClient{}
+}
+
+func (c *ClusterClient) MemberList(ctx context.Context, in *rpc.RequestMemberList, opts ...grpc.CallOption) (*rpc.ResponseMemberList, error) {
+	c.Lock()
+	defer c.Unlock()
+	c.MemberListCall++
+
+	return &rpc.ResponseMemberList{}, nil
+}
+
+func (c *ClusterClient) MemberStat(ctx context.Context, in *rpc.RequestMemberStat, opts ...grpc.CallOption) (*rpc.ResponseMemberStat, error) {
+	c.Lock()
+	defer c.Unlock()
+	c.MemberStatCall++
+
+	return &rpc.ResponseMemberStat{}, nil
+}
+
+func (c *ClusterClient) AgentList(ctx context.Context, in *rpc.RequestAgentList, opts ...grpc.CallOption) (*rpc.ResponseAgentList, error) {
+	c.Lock()
+	defer c.Unlock()
+	c.AgentListCall++
+
+	return &rpc.ResponseAgentList{}, nil
+}
+
+func (c *ClusterClient) DefragmentDatastore(ctx context.Context, in *rpc.RequestDefragmentDatastore, opts ...grpc.CallOption) (*rpc.ResponseDefragmentDatastore, error) {
+	c.Lock()
+	defer c.Unlock()
+	c.DefragmentDatastoreCall++
+
+	return &rpc.ResponseDefragmentDatastore{}, nil
+}
+
+type CertificateAuthorityClient struct {
+	sync.Mutex
+
+	GetSignedListCall    int
+	NewClientCertCall    int
+	NewServerCertCall    int
+	RevokeCall           int
+	GetCall              int
+	GetRevokedListCall   int
+	WatchRevokedListCall int
+}
+
+func NewCertificateAuthorityClient() *CertificateAuthorityClient {
+	return &CertificateAuthorityClient{}
+}
+
+func (c *CertificateAuthorityClient) GetSignedList(ctx context.Context, in *rpc.RequestGetSignedList, opts ...grpc.CallOption) (*rpc.ResponseGetSignedList, error) {
+	c.Lock()
+	defer c.Unlock()
+	c.GetSignedListCall++
+
+	return &rpc.ResponseGetSignedList{}, nil
+}
+
+func (c *CertificateAuthorityClient) NewClientCert(ctx context.Context, in *rpc.RequestNewClientCert, opts ...grpc.CallOption) (*rpc.ResponseNewClientCert, error) {
+	c.Lock()
+	defer c.Unlock()
+	c.NewClientCertCall++
+
+	return &rpc.ResponseNewClientCert{}, nil
+}
+
+func (c *CertificateAuthorityClient) NewServerCert(ctx context.Context, in *rpc.RequestNewServerCert, opts ...grpc.CallOption) (*rpc.ResponseNewServerCert, error) {
+	c.Lock()
+	defer c.Unlock()
+	c.NewServerCertCall++
+
+	return &rpc.ResponseNewServerCert{}, nil
+}
+
+func (c *CertificateAuthorityClient) Revoke(ctx context.Context, in *rpc.CARequestRevoke, opts ...grpc.CallOption) (*rpc.CAResponseRevoke, error) {
+	c.Lock()
+	defer c.Unlock()
+	c.RevokeCall++
+
+	return &rpc.CAResponseRevoke{}, nil
+}
+
+func (c *CertificateAuthorityClient) Get(ctx context.Context, in *rpc.CARequestGet, opts ...grpc.CallOption) (*rpc.CAResponseGet, error) {
+	c.Lock()
+	defer c.Unlock()
+	c.GetCall++
+
+	return &rpc.CAResponseGet{}, nil
+}
+
+func (c *CertificateAuthorityClient) GetRevokedList(ctx context.Context, in *rpc.RequestGetRevokedList, opts ...grpc.CallOption) (*rpc.ResponseGetRevokedList, error) {
+	c.Lock()
+	defer c.Unlock()
+	c.GetRevokedListCall++
+
+	return &rpc.ResponseGetRevokedList{}, nil
+}
+
+func (c *CertificateAuthorityClient) WatchRevokedCert(ctx context.Context, in *rpc.RequestWatchRevokedCert, opts ...grpc.CallOption) (rpc.CertificateAuthority_WatchRevokedCertClient, error) {
+	c.Lock()
+	defer c.Unlock()
+	c.WatchRevokedListCall++
+
+	return nil, nil
 }
