8c4a98cc6cb1874b6869f1c5a3a4c076ba1b907d
Fumihiro Ito <fmhrit@gmail.com>
2020-05-04 17:26:18 +0900

    Update RBAC policy

--- operator/pkg/controllers/proxy_controller.go
+++ operator/pkg/controllers/proxy_controller.go
@@ -46,6 +46,10 @@ import (
 // +kubebuilder:rbac:groups=proxy.f110.dev,resources=roles/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=proxy.f110.dev,resources=rpcpermissions,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=proxy.f110.dev,resources=rpcpermissions/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=etcd.f110.dev,resources=etcdclusters,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=*,resources=secrets;configmaps;services;cronjob;deployments;poddisruptionbudgets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=cert-manager.io,resources=certificates,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=monitoring.coreos.com,resources=podmonitors,servicemonitors,verbs=get;list;watch;create;update;patch;delete
 
 type Controller struct {
 	schema.GroupVersionKind
