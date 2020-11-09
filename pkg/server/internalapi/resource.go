9a26385b92e44bdbc54ad859466e1f8069d63ea1
Fumihiro Ito <fmhrit@gmail.com>
2020-11-09 23:31:00 +0900

    Require the client cert when request user's ssh key

--- pkg/server/internalapi/resource.go
+++ pkg/server/internalapi/resource.go
@@ -6,6 +6,7 @@ import (
 	"encoding/pem"
 	"io"
 	"net/http"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 	"go.uber.org/zap"
@@ -29,8 +30,8 @@ func NewResourceServer(config *configv2.Config, userDatabase database.UserDataba
 
 func (r *ResourceServer) Route(mux *httprouter.Router) {
 	mux.GET("/internal/publickey", r.PublicKey)
-	mux.GET("/users/:username/ssh_keys", r.SSHKeys)
-	mux.GET("/users/:username/gpg_key", r.GPGKey)
+	mux.GET("/users/:username/ssh_keys", r.requireAuthn(r.SSHKeys))
+	mux.GET("/users/:username/gpg_key", r.requireAuthn(r.GPGKey))
 }
 
 func (r *ResourceServer) PublicKey(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
@@ -45,6 +46,34 @@ func (r *ResourceServer) PublicKey(w http.ResponseWriter, _ *http.Request, _ htt
 
 	w.Header().Set("Content-Type", "application/x-pem-file")
 	w.Write(buf.Bytes())
+}
+
+func (r *ResourceServer) requireAuthn(next httprouter.Handle) func(http.ResponseWriter, *http.Request, httprouter.Params) {
+	return func(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
+		if req.TLS == nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		if len(req.TLS.PeerCertificates) == 0 {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		cert := req.TLS.PeerCertificates[0]
+		if time.Now().After(cert.NotAfter) || time.Now().Before(cert.NotBefore) {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		_, err := cert.Verify(x509.VerifyOptions{
+			Roots:     r.Config.CertificateAuthority.Local.CertPool,
+			KeyUsages: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		})
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		next(w, req, params)
+	}
 }
 
 func (r *ResourceServer) SSHKeys(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
