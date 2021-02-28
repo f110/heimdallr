ce1ffde834a2ce31fbd7145c3b6dafef51476fd3
Fumihiro Ito <fmhrit@gmail.com>
2021-02-28 14:54:57 +0900

    pkg/cert: Use testify package

--- pkg/cert/certificate_authority_test.go
+++ pkg/cert/certificate_authority_test.go
@@ -11,6 +11,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"software.sslmate.com/src/go-pkcs12"
 
 	"go.f110.dev/heimdallr/pkg/config/configv2"
@@ -56,127 +58,84 @@ func TestCertificateAuthority_NewClientCertificate(t *testing.T) {
 		"test",
 		"for testing",
 	)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	privateKey, clientCert, _, err := pkcs12.DecodeChain(data.P12, "test")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	switch privateKey.(type) {
 	case *rsa.PrivateKey:
 	default:
-		t.Fatal("Unexpected private key algorithm")
+		require.Fail(t, "Unexpected private key algorithm")
 	}
 
 	signedCert, err := ca.GetSignedCertificate(context.Background(), clientCert.SerialNumber)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
-	if signedCert.Certificate.Subject.CommonName != "test@example.com" {
-		t.Errorf("Unexpected common name: %s", signedCert.Certificate.Subject.CommonName)
-	}
+	assert.Equal(t, "test@example.com", signedCert.Certificate.Subject.CommonName)
 
-	if err := ca.Revoke(context.Background(), signedCert); err != nil {
-		t.Fatal(err)
-	}
+	err = ca.Revoke(context.Background(), signedCert)
+	require.NoError(t, err)
 
 	select {
 	case revoked := <-revokedCertEventCh:
-		if revoked.CommonName != "test@example.com" {
-			t.Fatalf("Got a revoked cert but unexpected common name: %s", revoked.CommonName)
-		}
+		require.Equal(t, "test@example.com", revoked.CommonName)
 	case <-time.After(time.Second):
-		t.Fatal("Expect getting a revoked cert via watch channel")
+		require.Fail(t, "Expect getting a revoked cert via watch channel")
 	}
 
 	revokedCerts, err := ca.GetRevokedCertificates(context.Background())
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(revokedCerts) != 1 {
-		t.Fatalf("Expect 1 revoked cert %d got", len(revokedCerts))
-	}
-	if revokedCerts[0].CommonName != "test@example.com" {
-		t.Fatalf("Got a revoked cert but unexpected common name: %s", revokedCerts[0].CommonName)
-	}
+	require.NoError(t, err)
+	require.Len(t, revokedCerts, 1)
+	assert.Equal(t, "test@example.com", revokedCerts[0].CommonName)
 }
 
 func TestCertificateAuthority_NewAgentCertificate(t *testing.T) {
 	ca := NewCertificateAuthority(memory.NewCA(), newCertificateAuthorityConfig(t))
 
 	data, err := ca.NewAgentCertificate(context.Background(), "test", "defaultpassword", "for testing")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	privateKey, clientCert, _, err := pkcs12.DecodeChain(data.P12, "defaultpassword")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	switch privateKey.(type) {
 	case *ecdsa.PrivateKey:
 	default:
-		t.Fatal("Unexpected private key algorithm")
+		require.Fail(t, "Unexpected private key algorithm")
 	}
 
 	signedCerts, err := ca.GetSignedCertificates(context.Background())
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(signedCerts) != 1 {
-		t.Fatalf("Expect to get 1 cert but %d", len(signedCerts))
-	}
+	require.NoError(t, err)
+	require.Len(t, signedCerts, 1)
 
 	signedCert, err := ca.GetSignedCertificate(context.Background(), clientCert.SerialNumber)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if signedCert.Comment != "for testing" {
-		t.Fatal("Success creating and getting certificate but unexpected result")
-	}
+	require.NoError(t, err)
+	assert.Equal(t, "for testing", signedCert.Comment)
 }
 
 func TestCertificateAuthority_NewServerCertificate(t *testing.T) {
 	ca := NewCertificateAuthority(memory.NewCA(), newCertificateAuthorityConfig(t))
 
 	c, _, err := ca.NewServerCertificate("test.example.com")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
-	if c.Subject.CommonName != "test.example.com" {
-		t.Fatalf("Unexpected common name: %s", c.Subject.CommonName)
-	}
+	assert.Equal(t, "test.example.com", c.Subject.CommonName)
 }
 
 func TestCertificateAuthority_SignCertificateRequest(t *testing.T) {
 	ca := NewCertificateAuthority(memory.NewCA(), newCertificateAuthorityConfig(t))
 
 	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	template := &x509.CertificateRequest{
 		Subject:  pkix.Name{CommonName: "test@example.com"},
 		DNSNames: []string{},
 	}
 	b, err := x509.CreateCertificateRequest(rand.Reader, template, privateKey)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	csr, err := x509.ParseCertificateRequest(b)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	c, err := ca.SignCertificateRequest(context.Background(), csr, "for testing", false, false)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if c.Certificate.Subject.CommonName != "test@example.com" {
-		t.Fatalf("Unexpected common name: %s", c.Certificate.Subject.CommonName)
-	}
+	require.NoError(t, err)
+	assert.Equal(t, "test@example.com", c.Certificate.Subject.CommonName)
 }
--- pkg/cert/util_test.go
+++ pkg/cert/util_test.go
@@ -11,6 +11,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"software.sslmate.com/src/go-pkcs12"
 
 	"go.f110.dev/heimdallr/pkg/config/configv2"
@@ -39,46 +41,32 @@ func TestCreateNewCertificateForClient(t *testing.T) {
 		t.Parallel()
 
 		serial, err := NewSerialNumber()
-		if err != nil {
-			t.Fatal(err)
-		}
+		require.NoError(t, err)
 
 		p12, _, err := CreateNewCertificateForClient(pkix.Name{CommonName: "test@f110.dev"}, serial, database.DefaultPrivateKeyType, database.DefaultPrivateKeyBits, "test", ca.Local)
-		if err != nil {
-			t.Fatalf("%+v", err)
-		}
+		require.NoError(t, err)
 
 		_, cert, _, err := pkcs12.DecodeChain(p12, "test")
-		if err != nil {
-			t.Fatal(err)
-		}
-		if cert.Subject.CommonName != "test@f110.dev" {
-			t.Errorf("CommonName is test@f110.dev: %s", cert.Subject.CommonName)
-		}
+		require.NoError(t, err)
+		assert.Equal(t, "test@f110.dev", cert.Subject.CommonName)
 	})
 
 	t.Run("RSA", func(t *testing.T) {
 		t.Parallel()
 
 		serial, err := NewSerialNumber()
-		if err != nil {
-			t.Fatal(err)
-		}
+		require.NoError(t, err)
 
 		p12, _, err := CreateNewCertificateForClient(pkix.Name{CommonName: "test@f110.dev"}, serial, "rsa", 4096, "test", ca.Local)
-		if err != nil {
-			t.Fatalf("%+v", err)
-		}
+		require.NoError(t, err)
 
 		privateKey, _, _, err := pkcs12.DecodeChain(p12, "test")
-		if err != nil {
-			t.Fatal(err)
-		}
+		require.NoError(t, err)
 
 		switch v := privateKey.(type) {
 		case *rsa.PrivateKey:
 		default:
-			t.Errorf("Unexpected private key type: %v", v)
+			require.Fail(t, "Unexpected private key type: %v", v)
 		}
 	})
 
@@ -86,28 +74,20 @@ func TestCreateNewCertificateForClient(t *testing.T) {
 		bits := []int{224, 256, 384, 521}
 
 		serial, err := NewSerialNumber()
-		if err != nil {
-			t.Fatal(err)
-		}
+		require.NoError(t, err)
 
 		for _, b := range bits {
 			p12, _, err := CreateNewCertificateForClient(pkix.Name{CommonName: "test@f110.dev"}, serial, "ecdsa", b, "test", ca.Local)
-			if err != nil {
-				t.Fatalf("%+v", err)
-			}
+			require.NoError(t, err)
 
 			privateKey, _, _, err := pkcs12.DecodeChain(p12, "test")
-			if err != nil {
-				t.Fatal(err)
-			}
+			require.NoError(t, err)
 
 			switch v := privateKey.(type) {
 			case *ecdsa.PrivateKey:
-				if v.Params().BitSize != b {
-					t.Errorf("private key type is expected but key bits is expected size: %d", v.Params().BitSize)
-				}
+				assert.Equal(t, b, v.Params().BitSize)
 			default:
-				t.Errorf("Unexpected private key type: %v", v)
+				assert.Fail(t, "Unexpected private key type")
 			}
 		}
 	})
@@ -123,9 +103,7 @@ func TestCreateCertificateAuthorityForConfig(t *testing.T) {
 			},
 		},
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 }
 
 func TestSigningCertificateRequest(t *testing.T) {
@@ -143,133 +121,79 @@ func TestSigningCertificateRequest(t *testing.T) {
 
 	for _, c := range cases {
 		b, err := os.ReadFile(c.File)
-		if err != nil {
-			t.Fatal(err)
-		}
+		require.NoError(t, err)
 		block, _ := pem.Decode(b)
-		if block == nil {
-			t.Fatal("testdata is not a pem format")
-		}
-		if block.Type != "CERTIFICATE REQUEST" {
-			t.Fatal("testdata is not a CSR")
-		}
+		require.NotNil(t, block)
+		require.Equal(t, "CERTIFICATE REQUEST", block.Type)
 
 		csr, err := x509.ParseCertificateRequest(block.Bytes)
-		if err != nil {
-			t.Fatal(err)
-		}
+		require.NoError(t, err)
 
 		signedCert, err := SigningCertificateRequest(csr, ca.Local)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if signedCert.Subject.CommonName != c.CommonName {
-			t.Errorf("CommonName of Subject is not %s: %s", c.CommonName, signedCert.Subject.CommonName)
-		}
-		if signedCert.Issuer.CommonName != "test" {
-			t.Errorf("CommonName of Issuer is not %s: %s", "test", signedCert.Issuer.CommonName)
-		}
+		require.NoError(t, err)
+		assert.Equal(t, c.CommonName, signedCert.Subject.CommonName)
+		assert.Equal(t, "test", signedCert.Issuer.CommonName)
 	}
 }
 
 func TestGenerateServerCertificate(t *testing.T) {
 	ca := createCAForTest(t)
 	serverCert, privateKey, err := GenerateServerCertificate(ca.Local.Certificate, ca.Local.PrivateKey, []string{"test-server.test.f110.dev", "internal.test.f110.dev"})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
-	if privateKey == nil {
-		t.Error("was not generate a private key")
-	}
-	if serverCert.Subject.CommonName != "test-server.test.f110.dev" {
-		t.Errorf("CommonName is test-server.test.f110.dev: %s", serverCert.Subject.CommonName)
-	}
+	assert.NotNil(t, privateKey)
+	assert.Equal(t, "test-server.test.f110.dev", serverCert.Subject.CommonName)
 }
 
 func TestCreateCertificateRequest(t *testing.T) {
 	csrByte, privateKey, err := CreatePrivateKeyAndCertificateRequest(pkix.Name{CommonName: "test@f110.dev"}, []string{""})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
-	if privateKey == nil {
-		t.Error("was not generate a private key")
-	}
+	assert.NotNil(t, privateKey)
 	block, _ := pem.Decode(csrByte)
-	if block == nil {
-		t.Fatal("CreateCertificateRequest was not returned pem encoded CSR")
-	}
-	if block.Type != "CERTIFICATE REQUEST" {
-		t.Fatalf("CreateCertificateRequest was returned pem bytes but not CERTIFICATE REQUEST: %s", block.Type)
-	}
+	require.NotNil(t, block)
+	assert.Equal(t, "CERTIFICATE REQUEST", block.Type)
 
 	csr, err := x509.ParseCertificateRequest(block.Bytes)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if csr.Subject.CommonName != "test@f110.dev" {
-		t.Errorf("CommonName is test@f110.dev: %s", csr.Subject.CommonName)
-	}
+	require.NoError(t, err)
+	assert.Equal(t, "test@f110.dev", csr.Subject.CommonName)
 }
 
 func TestPemEncode(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		f, err := os.CreateTemp("", "")
-		if err != nil {
-			t.Fatal(err)
-		}
+		require.NoError(t, err)
 		defer os.Remove(f.Name())
 
 		privateKey, err := ecdsa.GenerateKey(elliptic.P224(), rand.Reader)
-		if err != nil {
-			t.Fatal(err)
-		}
+		require.NoError(t, err)
 		b, err := x509.MarshalECPrivateKey(privateKey)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if err := PemEncode(f.Name(), "EC PRIVATE KEY", b, nil); err != nil {
-			t.Fatal(err)
-		}
+		require.NoError(t, err)
+		err = PemEncode(f.Name(), "EC PRIVATE KEY", b, nil)
+		require.NoError(t, err)
 
 		readed, err := os.ReadFile(f.Name())
-		if err != nil {
-			t.Fatal(err)
-		}
+		require.NoError(t, err)
 		block, rest := pem.Decode(readed)
-		if block == nil {
-			t.Fatal("failed decoding pem file")
-		}
-		if len(rest) != 0 {
-			t.Fatal("probably failed encoding pem file. found some trailing bytes which unexpected")
-		}
-		if block.Type != "EC PRIVATE KEY" {
-			t.Fatalf("expected block type is EC PRIVATE KEY: %s", block.Type)
-		}
+		require.NotNil(t, block)
+		require.Len(t, rest, 0)
+		require.Equal(t, "EC PRIVATE KEY", block.Type)
 	})
 
 	t.Run("Illegal header", func(t *testing.T) {
 		f, err := os.CreateTemp("", "")
-		if err != nil {
-			t.Fatal(err)
-		}
+		require.NoError(t, err)
 		defer os.Remove(f.Name())
 
 		err = PemEncode(f.Name(), "ILLEGAL HEADER:", []byte("illegal"), map[string]string{"illegal: ": "value"})
-		if err == nil {
-			t.Fatal("returns err is expected but not")
-		}
+		require.Error(t, err)
 
-		if _, err := os.Stat(f.Name()); !os.IsNotExist(err) {
-			t.Fatal("if can not encode to pem, should delete a temporary file but not deleted")
-		}
+		_, err = os.Stat(f.Name())
+		require.True(t, os.IsNotExist(err), "if can not encode to pem, should delete a temporary file but not deleted")
 	})
 
 	t.Run("Failed create file", func(t *testing.T) {
 		err := PemEncode("/unknown/notexist/file/path", "UNKNOWN PATH", []byte("illegal"), nil)
-		if err == nil {
-			t.Fatal("PemEncode is success but is not expect")
-		}
+		require.Error(t, err)
 	})
 }
