910027387b6786f88cc2649fe0d96740aec10d66
Fumihiro Ito <fmhrit@gmail.com>
2021-03-08 21:40:22 +0900

    release: Can override a tag

--- pkg/cmd/release/container.go
+++ pkg/cmd/release/container.go
@@ -11,7 +11,7 @@ import (
 	"golang.org/x/xerrors"
 )
 
-func containerReleaseCmd(repository, sha256File, tag string) error {
+func containerReleaseCmd(repository, sha256File, tag string, override bool) error {
 	if tag == "" || sha256File == "" {
 		return xerrors.New("tag and sha256 is mandatory")
 	}
@@ -25,15 +25,18 @@ func containerReleaseCmd(repository, sha256File, tag string) error {
 	if err != nil {
 		return xerrors.Errorf(": %w", err)
 	}
-	images, err := remote.List(repo, remote.WithAuthFromKeychain(authn.DefaultKeychain))
-	if err != nil {
-		return xerrors.Errorf(": %w", err)
-	}
-	for _, v := range images {
-		if v == tag {
-			return xerrors.Errorf("Container tag %s is already exists", v)
+	if !override {
+		images, err := remote.List(repo, remote.WithAuthFromKeychain(authn.DefaultKeychain))
+		if err != nil {
+			return xerrors.Errorf(": %w", err)
+		}
+		for _, v := range images {
+			if v == tag {
+				return xerrors.Errorf("Container tag %s is already exists", v)
+			}
 		}
 	}
+
 	ref, err := name.ParseReference(fmt.Sprintf("%s@%s", repository, sha256))
 	if err != nil {
 		return xerrors.Errorf(": %w", err)
@@ -56,15 +59,17 @@ func Container(rootCmd *cobra.Command) {
 	repository := "quay.io/f110"
 	sha256File := ""
 	tag := ""
+	override := false
 	containerRelease := &cobra.Command{
 		Use: "container",
 		RunE: func(_ *cobra.Command, _ []string) error {
-			return containerReleaseCmd(repository, sha256File, tag)
+			return containerReleaseCmd(repository, sha256File, tag, override)
 		},
 	}
 	containerRelease.Flags().StringVar(&repository, "repository", repository, "Container repository name")
 	containerRelease.Flags().StringVar(&sha256File, "sha256", sha256File, "A file that contains a hash of container (e,g, sha256:4041a17506561283c28f168a0a84608bfcfe4847f7ac71cbb0c2fd354d7d4a5b)")
 	containerRelease.Flags().StringVar(&tag, "tag", tag, "Tag name")
+	containerRelease.Flags().BoolVar(&override, "override", false, "Override a tag if exists")
 
 	rootCmd.AddCommand(containerRelease)
 }
