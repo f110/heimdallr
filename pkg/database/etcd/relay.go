fc07348bf24823a3411b59f6c93d82e99dec621b
Fumihiro Ito <fmhrit@gmail.com>
2021-03-29 00:23:07 +0900

    database/etcd: Use cache module in RelayLocator

--- pkg/database/etcd/relay.go
+++ pkg/database/etcd/relay.go
@@ -3,7 +3,6 @@ package etcd
 import (
 	"context"
 	"fmt"
-	"strings"
 	"sync"
 	"time"
 
@@ -17,58 +16,49 @@ import (
 )
 
 type RelayLocator struct {
-	client      *clientv3.Client
-	gone        chan *database.Relay
-	watchCancel context.CancelFunc
+	client *clientv3.Client
+	gone   chan *database.Relay
+	cache  *Cache
 
-	mu    sync.RWMutex
-	cache map[string]*database.Relay
-
+	mu             sync.RWMutex
 	myListenedAddr []string
 }
 
 var _ database.RelayLocator = &RelayLocator{}
 
-func NewRelayLocator(ctx context.Context, client *clientv3.Client) (*RelayLocator, error) {
-	watchCtx, cancel := context.WithCancel(context.Background())
+func NewRelayLocator(_ context.Context, client *clientv3.Client) (*RelayLocator, error) {
 	rl := &RelayLocator{
 		client:         client,
-		watchCancel:    cancel,
-		cache:          make(map[string]*database.Relay),
+		cache:          NewCache(client, "relay/", nil),
 		gone:           make(chan *database.Relay),
 		myListenedAddr: make([]string, 0),
 	}
-
-	res, err := client.Get(ctx, "relay/", clientv3.WithPrefix())
-	if err != nil {
-		return nil, xerrors.Errorf(": %v", err)
-	}
-	for _, v := range res.Kvs {
-		r := &database.Relay{}
-		if err := yaml.Unmarshal(v.Value, r); err != nil {
-			logger.Log.Warn("Ignore relay record due to unmarshal error", zap.Error(err), zap.String("key", string(v.Key)))
-			continue
-		}
-		if r.Name == "" {
-			logger.Log.Warn("Ignore relay caching", zap.String("key", string(v.Key)))
-		}
-		r.Version = v.Version
-		rl.cache[r.Name] = r
-	}
-
-	w := client.Watch(watchCtx, "relay/", clientv3.WithPrefix(), clientv3.WithRev(res.Header.Revision))
-	go rl.watch(w, res.Header.Revision)
+	rl.cache.Start(context.Background())
 
 	return rl, nil
 }
 
 func (l *RelayLocator) Get(name string) (*database.Relay, bool) {
 	// TODO: Support multiple relay
-	l.mu.RLock()
-	defer l.mu.RUnlock()
+	all, err := l.cache.All()
+	if err != nil {
+		return nil, false
+	}
 
-	v, ok := l.cache[name]
-	return v, ok
+	for _, v := range all {
+		r := &database.Relay{}
+		if err := yaml.Unmarshal(v.Value, r); err != nil {
+			logger.Log.Warn("Ignore relay record due to unmarshal error", zap.Error(err), zap.String("key", string(v.Key)))
+			continue
+		}
+		r.Version = v.Version
+
+		if r.Name == name {
+			return r, true
+		}
+	}
+
+	return nil, false
 }
 
 func (l *RelayLocator) Set(ctx context.Context, r *database.Relay) error {
@@ -88,7 +78,6 @@ func (l *RelayLocator) Set(ctx context.Context, r *database.Relay) error {
 
 	l.mu.Lock()
 	l.myListenedAddr = append(l.myListenedAddr, r.Addr)
-	l.cache[r.Name] = r
 	l.mu.Unlock()
 
 	return nil
@@ -128,7 +117,6 @@ func (l *RelayLocator) Delete(ctx context.Context, name, addr string) error {
 			l.myListenedAddr = append(l.myListenedAddr[:i], l.myListenedAddr[i+1:]...)
 		}
 	}
-	delete(l.cache, name)
 	l.mu.Unlock()
 
 	return nil
@@ -146,74 +134,24 @@ func (l *RelayLocator) GetListenedAddrs() []string {
 }
 
 func (l *RelayLocator) ListAllConnectedAgents() []*database.Relay {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-
-	result := make([]*database.Relay, 0, len(l.cache))
-	for _, v := range l.cache {
-		result = append(result, v)
+	result := make([]*database.Relay, 0, l.cache.Len())
+	all, err := l.cache.All()
+	if err != nil {
+		return nil
+	}
+	for _, v := range all {
+		r := &database.Relay{}
+		if err := yaml.Unmarshal(v.Value, r); err != nil {
+			logger.Log.Warn("Ignore relay record due to unmarshal error", zap.Error(err), zap.String("key", string(v.Key)))
+			continue
+		}
+		r.Version = v.Version
+		result = append(result, r)
 	}
 
 	return result
 }
 
-func (l *RelayLocator) watch(watch clientv3.WatchChan, startRev int64) {
-	logger.Log.Debug("Start watch relay locator")
-	defer logger.Log.Debug("Stop watch relay locator")
-
-	for {
-		select {
-		case events := <-watch:
-			if len(events.Events) == 0 {
-				return
-			}
-			if startRev >= events.Header.Revision {
-				continue
-			}
-
-			l.watchEvents(events.Events)
-		}
-	}
-}
-
-func (l *RelayLocator) watchEvents(events []*clientv3.Event) {
-	for _, e := range events {
-		switch e.Type {
-		case clientv3.EventTypePut:
-			r := &database.Relay{}
-			if err := yaml.Unmarshal(e.Kv.Value, r); err != nil {
-				continue
-			}
-			r.Version = e.Kv.Version
-
-			logger.Log.Debug("Add a relay to cache", zap.String("name", r.Name))
-			l.mu.Lock()
-			l.cache[r.Name] = r
-			l.mu.Unlock()
-		case clientv3.EventTypeDelete:
-			key := strings.Split(string(e.Kv.Key), "/")
-			name := key[len(key)-2]
-			if name == "" {
-				continue
-			}
-
-			if _, ok := l.cache[name]; !ok {
-				continue
-			}
-			logger.Log.Debug("Remove relay from cache", zap.String("name", name))
-			l.mu.Lock()
-			delete(l.cache, name)
-			l.mu.Unlock()
-
-			select {
-			case l.gone <- &database.Relay{Name: key[len(key)-2], Addr: key[len(key)-1]}:
-			default:
-				logger.Log.Debug("RelayLocator: gone channel is blocking")
-			}
-		}
-	}
-}
-
 func (l *RelayLocator) Close() {
-	l.watchCancel()
+	l.cache.Close()
 }
--- pkg/database/etcd/relay_test.go
+++ pkg/database/etcd/relay_test.go
@@ -7,103 +7,70 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.etcd.io/etcd/v3/clientv3"
-	"go.etcd.io/etcd/v3/mvcc/mvccpb"
-	"sigs.k8s.io/yaml"
 
 	"go.f110.dev/heimdallr/pkg/database"
 )
 
-func TestNewRelayLocator(t *testing.T) {
-	rl, err := NewRelayLocator(context.Background(), client)
-	require.NoError(t, err)
-	require.NotNil(t, rl)
-}
-
-func TestRelayLocator_GetAndSet(t *testing.T) {
-	rl, err := NewRelayLocator(context.Background(), client)
-	require.NoError(t, err)
-
-	err = rl.Set(context.Background(), &database.Relay{Name: t.Name(), Addr: "127.0.0.1:10000"})
-	require.NoError(t, err)
-
-	relay, ok := rl.Get(t.Name())
-	require.True(t, ok)
-	assert.Equal(t, "127.0.0.1:10000", relay.Addr)
-
-	addrs := rl.GetListenedAddrs()
-	assert.Len(t, addrs, 1)
-
-	relays := rl.ListAllConnectedAgents()
-	assert.Len(t, relays, 1)
-
-	err = rl.Delete(context.Background(), t.Name(), "127.0.0.1:10000")
-	require.NoError(t, err)
-
-	_, ok = rl.Get(t.Name())
-	require.False(t, ok)
-}
-
-func TestRelayLocator_Update(t *testing.T) {
-	rl, err := NewRelayLocator(context.Background(), client)
-	require.NoError(t, err)
-
-	updatedAt := time.Now().Add(-10 * time.Second)
-	r := &database.Relay{Name: t.Name(), Addr: "127.0.0.1:10000", UpdatedAt: updatedAt}
-	err = rl.Set(context.Background(), r)
-	require.NoError(t, err)
-	relay, ok := rl.Get(t.Name())
-	require.True(t, ok)
-
-	err = rl.Update(context.Background(), relay)
-	require.NoError(t, err)
-	require.NotEqual(t, updatedAt.Unix(), relay.UpdatedAt.Unix())
-}
-
-func TestRelayLocator_Watch(t *testing.T) {
-	rl, err := NewRelayLocator(context.Background(), client)
-	require.NoError(t, err)
-
-	started := make(chan struct{})
-	gotCh := make(chan *database.Relay)
-	goneCh := rl.Gone()
-	go func() {
-		close(started)
-		gone := <-goneCh
-		t.Logf("Got: %v", *gone)
-		gotCh <- gone
-	}()
-
-	// Wait for starting goroutine
-	select {
-	case <-started:
-	}
-
-	buf, err := yaml.Marshal(&database.Relay{Name: t.Name()})
-	require.NoError(t, err)
-	rl.watchEvents([]*clientv3.Event{
-		{
-			Type: clientv3.EventTypePut,
-			Kv:   &mvccpb.KeyValue{Value: buf},
-		},
+func TestRelayLocator(t *testing.T) {
+	t.Run("New", func(t *testing.T) {
+		rl, err := NewRelayLocator(context.Background(), client)
+		require.NoError(t, err)
+		require.NotNil(t, rl)
 	})
-	relay, ok := rl.Get(t.Name())
-	assert.True(t, ok)
-	assert.Equal(t, t.Name(), relay.Name)
 
-	rl.watchEvents([]*clientv3.Event{
-		{
-			Type: clientv3.EventTypeDelete,
-			Kv:   &mvccpb.KeyValue{Key: []byte("/" + t.Name() + "/127.0.0.1:10000")},
-		},
+	t.Run("GetAndSet", func(t *testing.T) {
+		rl, err := NewRelayLocator(context.Background(), client)
+		require.NoError(t, err)
+		ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+		defer cancel()
+		err = rl.cache.WaitForSync(ctx)
+		require.NoError(t, err)
+
+		notify := rl.cache.Notify()
+
+		err = rl.Set(context.Background(), &database.Relay{Name: t.Name(), Addr: "127.0.0.1:10000"})
+		require.NoError(t, err)
+		waitNotify(t, notify)
+
+		relay, ok := rl.Get(t.Name())
+		require.True(t, ok)
+		assert.Equal(t, "127.0.0.1:10000", relay.Addr)
+
+		addrs := rl.GetListenedAddrs()
+		assert.Len(t, addrs, 1)
+
+		relays := rl.ListAllConnectedAgents()
+		assert.Len(t, relays, 1)
+
+		err = rl.Delete(context.Background(), t.Name(), "127.0.0.1:10000")
+		require.NoError(t, err)
+		waitNotify(t, notify)
+
+		_, ok = rl.Get(t.Name())
+		require.False(t, ok)
 	})
-	_, ok = rl.Get(t.Name())
-	assert.False(t, ok)
 
-	select {
-	case got := <-gotCh:
-		assert.Equal(t, t.Name(), got.Name)
-	case <-time.After(10 * time.Second):
-		require.Fail(t, "Timeout")
-	}
+	t.Run("Update", func(t *testing.T) {
+		rl, err := NewRelayLocator(context.Background(), client)
+		require.NoError(t, err)
+		ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+		defer cancel()
+		err = rl.cache.WaitForSync(ctx)
+		require.NoError(t, err)
+
+		notify := rl.cache.Notify()
+
+		updatedAt := time.Now().Add(-10 * time.Second)
+		r := &database.Relay{Name: t.Name(), Addr: "127.0.0.1:10000", UpdatedAt: updatedAt}
+		err = rl.Set(context.Background(), r)
+		require.NoError(t, err)
+		waitNotify(t, notify)
+
+		relay, ok := rl.Get(t.Name())
+		require.True(t, ok)
+
+		err = rl.Update(context.Background(), relay)
+		require.NoError(t, err)
+		require.NotEqual(t, updatedAt.Unix(), relay.UpdatedAt.Unix())
+	})
 }
