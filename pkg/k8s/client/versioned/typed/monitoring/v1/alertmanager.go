76af9c824d6432b6d75f507eda930af79ff8283b
Fumihiro Ito <fmhrit@gmail.com>
2021-04-14 00:47:12 +0900

    *: Update BUILD file

--- pkg/k8s/client/versioned/typed/monitoring/v1/alertmanager.go
+++ pkg/k8s/client/versioned/typed/monitoring/v1/alertmanager.go
@@ -32,7 +32,7 @@ import (
 	"time"
 
 	v1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
-	scheme "go.f110.dev/heimdallr/operator/pkg/client/versioned/scheme"
+	scheme "go.f110.dev/heimdallr/pkg/k8s/client/versioned/scheme"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
