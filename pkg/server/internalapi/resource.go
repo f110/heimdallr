b0bd93746f0ff91a8b67abb9eef3aa2928d5da19
Fumihiro Ito <fmhrit@gmail.com>
2021-06-17 11:48:35 +0900

    internalapi: Remove the feature that providing SSH key

--- pkg/server/internalapi/resource.go
+++ pkg/server/internalapi/resource.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"crypto/x509"
 	"encoding/pem"
-	"io"
 	"net/http"
 	"time"
 
@@ -30,7 +29,6 @@ func NewResourceServer(config *configv2.Config, userDatabase database.UserDataba
 
 func (r *ResourceServer) Route(mux *httprouter.Router) {
 	mux.GET("/internal/publickey", r.PublicKey)
-	mux.GET("/users/:username/ssh_keys", r.requireAuthn(r.SSHKeys))
 }
 
 func (r *ResourceServer) PublicKey(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
@@ -73,31 +71,4 @@ func (r *ResourceServer) requireAuthn(next httprouter.Handle) func(http.Response
 
 		next(w, req, params)
 	}
-}
-
-func (r *ResourceServer) SSHKeys(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
-	loginName := params.ByName("username")
-	if loginName == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	identity := loginName
-	id, err := r.userDatabase.GetIdentityByLoginName(req.Context(), loginName)
-	// If passed identity, not login name, GetIdentityByLoginName will return ErrUserNotFound.
-	// At this point, ErrUserNotFound is ignored because "username" may be the identity.
-	if err != nil && err != database.ErrUserNotFound {
-		logger.Log.Warn("Failed get identity", zap.Error(err), zap.String("login_name", loginName))
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	if id != "" {
-		identity = id
-	}
-
-	keys, err := r.userDatabase.GetSSHKeys(req.Context(), identity)
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	io.WriteString(w, keys.Keys)
 }
