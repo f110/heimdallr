8b674373e79a97ad49adb40f3b157fd6587ccb1d
Fumihiro Ito <fmhrit@gmail.com>
2023-01-01 11:58:34 +0900

    build/release: Running release flow on CI

--- build/rules/release.bash
+++ build/rules/release.bash
@@ -8,4 +8,9 @@ BRANCH=@@BRANCH@@
 ASSETS=@@ASSETS@@
 BODY=@@BODY@@
 
-$BIN github --version $VERSION --repo $REPO --from $BRANCH --body "$BODY" "${ASSETS[@]}"
+GITHUB_APP_ARG=""
+if [ -n "$GITHUB_PRIVATE_KEY" ] && [ -n "$GITHUB_APP_ID" ] && [ -n "$GITHUB_INSTALLATION_ID" ]; then
+    GITHUB_APP_ARG="--github-app-id $GITHUB_APP_ID --github-installation-id $GITHUB_INSTALLATION_ID --github-private-key $GITHUB_PRIVATE_KEY"
+fi
+
+$BIN github --version $VERSION --repo $REPO --from $BRANCH --body "$BODY" $GITHUB_APP_ARG "${ASSETS[@]}"
